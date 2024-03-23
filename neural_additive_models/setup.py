eb5bb31629a634b29d6a9556146133f069a647e9
debesheedas <66054332+debesheedas@users.noreply.github.com>
2024-03-23 12:21:04 +0100

    Update setup.py

--- neural_additive_models/setup.py
+++ neural_additive_models/setup.py
@@ -25,7 +25,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 install_requires = [
     'tensorflow>=1.15',
     'numpy>=1.15.2',
-    'sklearn',
+    'scikit-learn',
     'pandas>=0.24',
     'absl-py',
 ]
@@ -38,7 +38,7 @@ setup(
     description=nam_description,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/agarwl/google-research/tree/master/neural_additive_models',
+    url='https://github.com/debesheedas/google-research/tree/master/neural_additive_models',
     author='Rishabh Agarwal',
     classifiers=[
         'Development Status :: 4 - Beta',
